f3fa6ae49ea102dcf09f52c0f0a0794c102d9fbd
ZeroBounce <34064209+zerobounce@users.noreply.github.com>
2018-07-17 17:28:06 -0400

    Update zerobounce.php

--- zerobounce.php
+++ zerobounce.php
@@ -1,7 +1,7 @@
 <?php
 class ZeroBounceAPI {
   private $key;
-  private $baseURL = "https://api.zerobounce.net/v1/";
+  private $baseURL = "https://api.zerobounce.net/v2/";
   function __construct($key){
     $this->key = $key;
   }
@@ -9,7 +9,7 @@ class ZeroBounceAPI {
   *  this function is used for authentication and http api calls
   */
   private function api_call($method, array $params){
-    $params['apikey'] = $this->key;
+    $params['api_key'] = $this->key;
     $paramsURI = http_build_query($params);
     $url = "{$this->baseURL}{$method}?{$paramsURI}";
     if(!isset($this->ch)){
@@ -36,16 +36,11 @@ class ZeroBounceAPI {
   public function get_credits(){
     return $this->api_call("getcredits",[]);
   }
-  /**
-  * wrapper for api_call/validate
-  */
-  public function validate($email){
-    return $this->api_call("validate",['email' => $email]);
-  }
+
   /**
   *  wrapper for api_call/validate
   */
-  public function validatewithip($email, $ip){
-    return $this->api_call("validatewithip",["email" => $email, "ipAddress" => $ip]);
+  public function validate($email, $ip){
+    return $this->api_call("validate",["email" => $email, "ip_address" => $ip]);
   }
 }
